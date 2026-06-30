5085b13b17f10e3a3cfac9880fe0d2b78930d71c
shanezins <shanezins@gmail.com>
2026-06-30 08:13:53 -0400

    new

--- 3_DOF_Rocket_TVC_Simulation/scripts/run_simulation.m
+++ 3_DOF_Rocket_TVC_Simulation/scripts/run_simulation.m
@@ -4,13 +4,9 @@ close all
 
 % 2D 3DOF Rocket Takeoff Simulation with Thrust Vector Control
 
-scriptDir = fileparts(mfilename('fullpath'));
-projectDir = fileparts(scriptDir);
-modelDir = fullfile(projectDir, 'model');
-
 % Target position
-x_target = -50;      % desired horizontal position, m
-z_target = 150;      % desired altitude, m
+x_target = 8;      % desired horizontal position, m
+z_target = 113;      % desired altitude, m
 
 % Rocket parameters
 m = 100;             % mass, kg
@@ -35,6 +31,23 @@ Kvx = 0.025;         % horizontal velocity damping gain
 Kx = 0.002;          % horizontal position gain
 
 % Run Simulink model
-modelName = 'rocket_3dof';  
+modelName = 'rocket_3dof';
+
+scriptDir = fileparts(mfilename('fullpath'));
+projectDir = fileparts(scriptDir);
+modelDir = fullfile(projectDir, 'model');
+modelPath = fullfile(modelDir, [modelName '.slx']);
+
+disp("Looking for model here:")
+disp(modelPath)
+
+if ~isfile(modelPath)
+    error('Could not find model file here: %s', modelPath)
+end
+
+addpath(modelDir)
+load_system(modelPath)
+
 out = sim(modelName, 'StopTime', num2str(t_final));
+
 disp('Simulation complete.')
